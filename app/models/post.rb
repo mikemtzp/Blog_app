cef91bc26476a8a26fa7626137dcad41f94ebe63
mikemtzp <mikemp8226@gmail.com>
2022-09-14 19:41:34 -0500

    Create custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  # A method that updates the posts counter for a user.
+  def update_post_counter
+    user = User.find(author_id)
+    user.update(posts_counter: Post.where(author_id:).count)
+  end
+
+  # A method which returns the 5 most recent comments for a given post.
+  def recent_comments
+    Comment.where(post_id: self).order('created_at DESC').limit(5)
+  end
 end
